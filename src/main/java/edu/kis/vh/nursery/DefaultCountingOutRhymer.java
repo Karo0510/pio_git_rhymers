db89e829fe20bd66c629ae401c74cc95188a4d5a
Karolina Maciejewska <karma0510@gmail.com>
2020-01-14 00:56:02 +0100

    Format

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,31 +6,26 @@ public class DefaultCountingOutRhymer {
 
     public int total = -1;
 
-    public void countIn(int in)
-    {
+    public void countIn(int in) {
         if (!isFull())
             NUMBERS[++total] = in;
     }
 
-    public boolean callCheck()
-    {
+    public boolean callCheck() {
         return total == -1;
     }
 
-    public boolean isFull()
-    {
+    public boolean isFull() {
         return total == 11;
     }
 
-    protected int peekaboo()
-    {
+    protected int peekaboo() {
         if (callCheck())
             return -1;
         return NUMBERS[total];
     }
 
-    public int countOut()
-    {
+    public int countOut() {
         if (callCheck())
             return -1;
         return NUMBERS[total--];
