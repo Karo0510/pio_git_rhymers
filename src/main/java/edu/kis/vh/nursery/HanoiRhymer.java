9280571e26d96c34a221b015ca5fe15c2ec7b8ec
PrezesMajkel <prezesmajkel43@gmail.com>
2020-01-28 11:09:59 +0100

    Poprwaki nazewnictwa i formatowania

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -5,9 +5,11 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
     int totalRejected = 0;
 
     public int reportRejected() {
+
         return totalRejected;
     }
 
+    @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
