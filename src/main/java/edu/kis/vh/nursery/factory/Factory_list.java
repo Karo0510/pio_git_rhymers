be0162d5f57e58c53bc1917edb55f7a716379482
Karolina Maciejewska <karma0510@gmail.com>
2020-01-28 21:02:20 +0100

    proba 2.3

---
+++ src/main/java/edu/kis/vh/nursery/factory/Factory_list.java
@@ -0,0 +1,25 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+
+public class Factory1 implements RhymersFactory {
+    @Override
+    public DefaultCountingOutRhymer getStandardRhymer() {
+        return null;
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFalseRhymer() {
+        return null;
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFIFORhymer() {
+        return null;
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getHanoiRhymer() {
+        return null;
+    }
+}
